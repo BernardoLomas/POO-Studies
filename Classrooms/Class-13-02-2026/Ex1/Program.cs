f5b666a0aef4bfdab876ca33c0704a9010bfd9d8
BernardoLomas <bernardo.lomasb@gmail.com>
2026-02-13 19:45:54 -0300

    Exercise 01: Temperature Analysis

---
+++ Classrooms/Class-13-02-2026/Ex1/Program.cs
@@ -0,0 +1,56 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine($"How many days does you want to register?");
+        int days = int.Parse(Console.ReadLine());
+
+        double[] temperature = new double[days];
+        double cont = 0;
+
+        for (int i = 0; i < temperature.Length; i++)
+        {
+            Console.WriteLine($"Write the temperature of day {i + 1}: ");
+            temperature[i] = double.Parse(Console.ReadLine());
+            cont += temperature[i];
+        }
+
+        var A = Media(cont, days);
+        var B = Highest(temperature);
+        var C = Smallest(temperature);
+
+        Console.WriteLine($"Media: {A:F2}");
+        Console.WriteLine($"Highest temperature: {B:F2}");
+        Console.WriteLine($"Smallest temperature: {C:F2}");
+    }
+
+    static double Media(double cont, int days)
+    {
+        return cont / days;
+    }
+
+    static double Highest(double[] temperature)
+    {
+        double highest = temperature[0];
+
+        for (int i = 0; i < temperature.Length; i++)
+        {
+            if (temperature[i] > highest)
+                highest = temperature[i];
+        }
+
+        return highest;
+    }
+    static double Smallest(double[] temperature)
+    {
+        double smallest = temperature[0];
+
+        for (int i = 0; i < temperature.Length; i++)
+        {
+            if (temperature[i] < smallest)
+                smallest = temperature[i];
+        }
+
+        return smallest;
+    }
+}
