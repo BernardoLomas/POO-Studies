74698ff228f4a5b19e90851652e12a495b48a76e
BernardoLomas <bernardo.lomasb@gmail.com>
2026-02-13 20:38:21 -0300

    Exercise 04: Math power with while

---
+++ Classrooms/Class-13-02-2026/Ex4/Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine($"Type the integer number: ");
+        int number = int.Parse(Console.ReadLine());
+        Console.WriteLine($"Type the expoent: ");
+        int exponent = int.Parse(Console.ReadLine());
+
+        var A = Calc(number, exponent);
+        Console.WriteLine($"The result of {number} to the power of {exponent} is: {A}");
+
+    }
+
+    static int Calc(int number, int exponent)
+    {
+        int cont = 1;
+        int result = number;
+
+        while(cont != exponent)
+        {
+            result = result * number;
+            cont++;
+        }
+
+        return result;
+    }
+}
