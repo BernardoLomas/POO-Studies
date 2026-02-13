37b7c0b0a9421c7cc4b6b387369f6a453be97178
BernardoLomas <bernardo.lomasb@gmail.com>
2026-02-13 20:11:39 -0300

    Exercise 3: Passwords validation

---
+++ Classrooms/Class-13-02-2026/Ex3/Program.cs
@@ -0,0 +1,25 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine($"Register a password: ");
+        string password = Console.ReadLine();
+
+        Console.WriteLine($"Confirm password: ");
+        string confirm = Console.ReadLine();
+
+        var A = Confirm(password, confirm);
+        if(A)
+            Console.WriteLine($"Password confirmed!");
+        else
+            Console.WriteLine($"Passwords are not the same!");
+    }
+
+    static bool Confirm(string password, string confirm)
+    {
+        if(password == confirm)
+            return true;
+        else
+            return false;
+    }   
+}
