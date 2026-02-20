ec668e7b54ab9e99b46bba7bb754a3d8b86e4c3d
BernardoLomas <bernardo.lomasb@gmail.com>
2026-02-20 19:44:05 -0300

    Exercise 01: Factorial number

---
+++ Classrooms/Class-20-02-2026/Ex1/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine($"What factorial number you want?");
+        int factorial = int.Parse(Console.ReadLine());
+
+        var A = Factorial(factorial);
+        Console.WriteLine($"Result: {A}");
+    }
+
+    static int Factorial(int factorial)
+    {
+        if (factorial <= 1)
+            return 1;
+        else
+            return factorial * Factorial(factorial - 1);
+    }
+}
