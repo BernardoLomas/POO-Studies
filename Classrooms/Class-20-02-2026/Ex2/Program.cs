5d3277b8b3e3cb502f627f3e591182f7ff6f551d
BernardoLomas <bernardo.lomasb@gmail.com>
2026-02-21 23:49:16 -0300

    Exercise 02: Multiple numbers

---
+++ Classrooms/Class-20-02-2026/Ex2/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine($"Type the number which you want to know the multiples: ");
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine($"How many multiples of {n} you want? ");
+        int q = int.Parse(Console.ReadLine());
+        
+        var A = Multiples(n, q);
+        Console.WriteLine($"")
+
+    }
+
+    static int Multiples(int n, int q)
+    {
+        if (n <= 1)
+            return Multiples(n);
+        else 
+            return Multiples(n) * Multiples(q - 1);
+    }
+}
