6d1dcf1cb7c2419a67f9b49e87fa728960e7dba9
BernardoLomas <bernardo.lomasb@gmail.com>
2026-03-01 22:26:28 -0300

    Exercise 05: Converter para binário

---
+++ Tasks/Task01-03-2026/Ex5/Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um número natural: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.Write($"{n} convertido para binário: ");
+        ConverteBinario(n);
+    }
+
+    static void ConverteBinario(int n)
+    {
+        if (n == 0)
+            return;
+
+        ConverteBinario(n / 2);
+
+        Console.Write(n % 2);
+    }
+}
