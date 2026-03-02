414488a72f9bf49c544922e2e7bce3fe649f94f7
BernardoLomas <bernardo.lomasb@gmail.com>
2026-03-01 21:42:58 -0300

    Exercise 02: Mostra múltiplos

---
+++ Tasks/Task01-03-2026/Ex2/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite o valor de n: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite o valor de q: ");
+        int q = int.Parse(Console.ReadLine());
+
+        ExibirMultiplos(n, q);
+    }
+
+    static void ExibirMultiplos(int n, int q)
+    {
+        if (q == 0)
+            return;
+
+        ExibirMultiplos(n, q - 1);
+
+        Console.WriteLine($"{q} múltiplo: {n} x {q} = {n * q}");
+    }
+}
