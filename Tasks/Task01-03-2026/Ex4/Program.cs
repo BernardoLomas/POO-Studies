abde2b4da0bf87d2b56791769bd7e77581627250
BernardoLomas <bernardo.lomasb@gmail.com>
2026-03-01 22:19:16 -0300

    Exercise 04: Soma de elementos do vetor

---
+++ Tasks/Task01-03-2026/Ex4/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int[] vetor = { 1, 1, 1, 1 };
+
+        int soma = SomaVetor(vetor, 0);
+
+        Console.WriteLine($"Soma dos elementos: {soma}");
+    }
+
+    static int SomaVetor(int[] vetor, int indice)
+    {
+        if (indice == vetor.Length)
+            return 0;
+
+        return vetor[indice] + SomaVetor(vetor, indice + 1);
+    }
+}
