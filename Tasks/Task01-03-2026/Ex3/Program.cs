f41e2a3722d11b50a157cd4298716fa0d9755fd0
BernardoLomas <bernardo.lomasb@gmail.com>
2026-03-01 22:06:19 -0300

    Exercise 03: Serie harmônica

---
+++ Tasks/Task01-03-2026/Ex3/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite o número de termos: ");
+        int n = int.Parse(Console.ReadLine());
+
+        double resultado = Serie(n);
+
+        Console.WriteLine($"Resultado da série com {n} termos: {resultado}");
+    }
+
+    static double Serie(int n)
+    {
+        if (n == 1)
+            return 1.0;
+
+        return (1.0 / n) + Serie(n - 1);
+    }
+}
