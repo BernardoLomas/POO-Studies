538458b2319c99aa7afec5835ce082198b9fae1d
BernardoLomas <bernardo.lomasb@gmail.com>
2026-03-01 21:18:54 -0300

    Exercise 01: Soma Recursiva

---
+++ Tasks/Task01-03-2026/Ex1/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine($"Digite o primeiro valor: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Digite o valor a ser multiplicado: ");
+        int m = int.Parse(Console.ReadLine());
+
+        var A = SomaRecursiva(n, m);
+
+        Console.WriteLine($"O resultado da multiplicação de {n} x {m} é: {A}");
+    
+    }
+
+    static int SomaRecursiva(int n, int m)
+    {
+        if(m == 0)
+            return 0;
+        else
+            return n + SomaRecursiva(n, m - 1);
+    }
+}
