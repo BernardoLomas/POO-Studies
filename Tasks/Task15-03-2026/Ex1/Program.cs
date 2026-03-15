3e1a751bb7f96c90c7298a437cf8cb9d602f37f8
BernardoLomas <bernardo.lomasb@gmail.com>
2026-03-15 19:28:21 -0300

    Exercise 01: Classes e Métodos

---
+++ Tasks/Task15-03-2026/Ex1/Program.cs
@@ -0,0 +1,67 @@
+﻿class ContaBancaria
+{
+    private string Titular;
+    private double Saldo;
+    public string GetTitular()
+    {
+        return Titular;
+    }
+
+    public double GetSaldo()
+    {
+        return Saldo;
+    }
+
+    public void SetTitular(string titular)
+    {
+        Titular = titular;
+    }
+
+    public void SetSaldo(double saldo)
+    {
+        Saldo = saldo;
+    }
+
+    public void Depositar(double valor)
+    {
+        Saldo += valor;
+    }
+
+    public void Sacar(double valor)
+    {
+        if (valor > Saldo)
+        {
+            Console.WriteLine($"Saldo insuficiente para realizar o saque!");
+        }
+        else 
+            Saldo -= valor;
+    }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        ContaBancaria conta = new ();
+
+        Console.WriteLine($"Digite o nome do titular: ");
+        string nome = Console.ReadLine();
+        conta.SetTitular("Bernardo Lomas");
+
+        Console.WriteLine($"Digite o saldo inicial: ");
+        double saldo = double.Parse(Console.ReadLine());
+        conta.SetSaldo(1000);
+
+        Console.WriteLine("Digite o valor para depósito: ");
+        double deposito = double.Parse(Console.ReadLine());
+        conta.Depositar(500);
+
+        Console.WriteLine($"Digite o valor para saque: ");
+        double saque = double.Parse(Console.ReadLine());
+        conta.Sacar(20);
+
+        Console.WriteLine($"Titular: " + conta.GetTitular());
+        Console.WriteLine($"Saldo: " + conta.GetSaldo());
+
+    }
+}
