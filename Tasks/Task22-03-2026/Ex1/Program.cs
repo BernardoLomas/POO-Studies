cb23be9058696825be0fee3ffa064e267defa6df
BernardoLomas <bernardo.lomasb@gmail.com>
2026-03-22 20:52:45 -0300

    Exercise 01: Static methods

---
+++ Tasks/Task22-03-2026/Ex1/Program.cs
@@ -0,0 +1,32 @@
+﻿static class ConversorTemperatura
+{
+    static public double CelsiusParaFahrenheit(double c)
+    {
+        return (c * 9/5) + 32;
+    }
+
+    static public double FahrenheitParaCelsius(double f)
+    {
+        return (f - 32) * 5/9;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine($"Escreva um valor para converter de Celsius para Fahrenheit: ");
+        double c = double.Parse(Console.ReadLine());
+        double resultadoF = ConversorTemperatura.CelsiusParaFahrenheit(c);
+        Console.WriteLine($"Resultado: {resultadoF} graus Fahrenheit");
+        
+
+        Console.WriteLine($"Escreva um valor para converter de Fahrenheit para Celsius: ");
+        double f = double.Parse(Console.ReadLine());
+        double resultadoC = ConversorTemperatura.FahrenheitParaCelsius(f);
+        Console.WriteLine($"Resultado: {resultadoC} graus Celsius");
+        
+
+
+    }
+}
