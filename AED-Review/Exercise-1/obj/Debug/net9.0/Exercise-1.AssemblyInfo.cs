604211e3afda5f24806da5ce2ddc906f88bd850a
BernardoLomas <bernardo.lomasb@gmail.com>
2026-04-19 01:44:50 -0300

    Exercise 01: Dependency

--- AED-Review/Exercise-1/obj/Debug/net9.0/Exercise-1.AssemblyInfo.cs
+++ AED-Review/Exercise-1/obj/Debug/net9.0/Exercise-1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercise-1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1f909fb38fdebb16c2fd3bea0ea8fa850f29a88")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+378985e95f5143e17177be36cfdaf31cc52e20dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercise-1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercise-1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Tasks/Task19-04-2026/Ex1/Program.cs
@@ -0,0 +1,49 @@
+﻿class Aluno
+{
+    private string nome;
+
+    public string GetNome()
+    {
+        return nome;
+    }
+
+    public void SetNome(string nome)
+    {
+        this.nome = nome;
+    }
+
+    public Aluno(string nome)
+    {
+        this.nome = nome;
+    }
+
+    public void ExibirNome()
+    {
+        Console.WriteLine($"Nome do aluno: {nome}");
+    }
+}
+
+class EscolaDependencia
+{
+    public void ExibirNomeReferencia(Aluno aluno)
+    {
+        Console.WriteLine($"Nome do aluno (Impresso por referência de classe): {aluno.GetNome()}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        EscolaDependencia escola = new EscolaDependencia();
+        Aluno aluno1 = new Aluno("Bernardo Lomas");
+        Aluno aluno2 = new Aluno("Otavio Lomas");
+
+        aluno1.ExibirNome();
+        aluno2.ExibirNome();
+
+        escola.ExibirNomeReferencia(aluno1);
+        escola.ExibirNomeReferencia(aluno2);
+
+    }
+}
